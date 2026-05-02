676d21cf2302e71d23b0eebbe04d6d5e3f20f669
Kavi Subramanyan <kavi.lee.subramanyan@gmail.com>
2026-05-02 12:01:48 -0500

    GetPieceAt_NewBoardRow0Col0_ThrowsException passes

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -1,8 +1,16 @@
 package domain;
 
+import domain.piece.Piece;
+
+import java.util.NoSuchElementException;
+
 public class Board {
 
     public boolean isEmpty(Position position) {
         return true;
+    }
+
+    public Piece getPieceAt(Position position) {
+        throw new NoSuchElementException("Cannot get piece at empty position");
     }
 }
--- src/test/java/domain/BoardTest.java
+++ src/test/java/domain/BoardTest.java
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
+
+    /*
+    * isEmpty() unit tests
+    * */
 
     @ParameterizedTest
     @CsvSource({
@@ -26,6 +32,28 @@ public class BoardTest {
         boolean actual = board.isEmpty(position);
 
         assertTrue(actual);
+    }
+
+    /*
+    * getPieceAt() unit tests
+    * */
+
+    @Test
+    public void GetPieceAt_NewBoardRow0Col0_ThrowsException() {
+        Board board = new Board();
+        Position position = EasyMock.createMock(Position.class);
+
+        Exception exception = assertThrows(
+            NoSuchElementException.class,
+            () -> board.getPieceAt(position)
+        );
+
+        assertTrue(board.isEmpty(position));
+
+        String expected = "Cannot get piece at empty position";
+        String actual = exception.getMessage();
+        assertEquals(expected, actual);
+
     }
 
 }
