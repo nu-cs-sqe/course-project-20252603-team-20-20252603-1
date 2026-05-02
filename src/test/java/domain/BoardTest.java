d57a877adddc6e786e489f806348ee486427f0e6
Kavi Subramanyan <kavi.lee.subramanyan@gmail.com>
2026-05-02 11:48:44 -0500

    parameterized TC1-7 and IsEmpty_NewBoardRowColBounds_IsTrue passes

--- src/test/java/domain/BoardTest.java
+++ src/test/java/domain/BoardTest.java
@@ -2,12 +2,24 @@ package domain;
 
 import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BoardTest {
 
-    @Test
-    public void IsEmpty_NewBoardRow0Col0_IsTrue() {
+    @ParameterizedTest
+    @CsvSource({
+        "0, 0",
+        "1, 0",
+        "6, 0",
+        "7, 0",
+        "0, 1",
+        "0, 6",
+        "0, 7",
+    })
+    public void IsEmpty_NewBoardRowColBounds_IsTrue(int row, int col) {
         Board board = new Board();
         Position position = EasyMock.createMock(Position.class);
 
