b657c12e040eaebe45590c4df1fff29a727b3a59
Kavi Subramanyan <kavi.lee.subramanyan@gmail.com>
2026-05-03 17:18:28 -0500

    refactor w/ checkstyle lint

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -3,39 +3,38 @@ package domain;
 import domain.piece.Color;
 import domain.piece.Piece;
 import domain.piece.PieceType;
-
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
 public class Board {
 
-    private Piece[][] _squares = new Piece[8][8];
+  private final Piece[][] squares = new Piece[8][8];
 
-    private Optional<Piece> _pieceAt(Position position) {
-        return Optional.ofNullable(_squares[position.getRow()][position.getCol()]);
+  private Optional<Piece> pieceAt(Position position) {
+    return Optional.ofNullable(squares[position.getRow()][position.getCol()]);
+  }
+
+  public void initializeBoard() {
+    // white pieces
+    for (int col = 0; col < 8; ++col) {
+      squares[0][col] = new Piece(PieceType.ROOK, Color.WHITE);
+      squares[1][col] = new Piece(PieceType.PAWN, Color.WHITE);
     }
 
-    public void initializeBoard() {
-        // white pieces
-        for(int col = 0; col < 8; ++col) {
-            _squares[0][col] = new Piece(PieceType.ROOK, Color.WHITE);
-            _squares[1][col] = new Piece(PieceType.PAWN, Color.WHITE);
-        }
-
-        // black pieces
-        for(int col = 0; col < 8; ++col) {
-            _squares[7][col] = new Piece(PieceType.ROOK, Color.BLACK);
-            _squares[6][col] = new Piece(PieceType.PAWN, Color.BLACK);
-        }
+    // black pieces
+    for (int col = 0; col < 8; ++col) {
+      squares[7][col] = new Piece(PieceType.ROOK, Color.BLACK);
+      squares[6][col] = new Piece(PieceType.PAWN, Color.BLACK);
     }
+  }
 
-    public boolean isEmpty(Position position) {
-        return _pieceAt(position).isEmpty();
-    }
+  public boolean isEmpty(Position position) {
+    return pieceAt(position).isEmpty();
+  }
 
-    public Piece getPieceAt(Position position) {
-        return _pieceAt(position).orElseThrow(
-            () -> new NoSuchElementException("Cannot get piece at empty position")
-        );
-    }
+  public Piece getPieceAt(Position position) {
+    return pieceAt(position).orElseThrow(
+        () -> new NoSuchElementException("Cannot get piece at empty position")
+    );
+  }
 }
