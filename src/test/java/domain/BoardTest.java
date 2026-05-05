d293e08909520f35eb00db3ade8191846ad53255
Kavi Subramanyan <kavi.lee.subramanyan@gmail.com>
2026-05-05 12:36:55 -0500

    GetPieceAt_AfterInitRowXColY_CorrectPiece(1, 7, WHITE, KNIGHT) passes

--- src/test/java/domain/BoardTest.java
+++ src/test/java/domain/BoardTest.java
@@ -74,6 +74,7 @@ public class BoardTest {
       "2, 1, WHITE, PAWN",
       "7, 1, BLACK, PAWN",
       "1, 2, WHITE, KNIGHT",
+      "1, 7, WHITE, KNIGHT"
   })
   public void GetPieceAt_AfterInitRowXColY_CorrectPiece(int row,
                                                         int col,
