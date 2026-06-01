cf5ba59d1caeb9806ead43b0d8432aa02b01e959
domingoc1232 <152648268+domingoc1232@users.noreply.github.com>
2026-06-01 16:02:05 -0500

    GetCandidateMoves_Black_Row8_Col1_Returns21Candidates passes

--- src/test/java/domain/piece/QueenTest.java
+++ src/test/java/domain/piece/QueenTest.java
@@ -103,4 +103,28 @@ public class QueenTest {
         assertEquals(21, actual.size());
         assertTrue(actual.containsAll(expected));
     }
+
+    @Test
+    public void GetCandidateMoves_Black_Row8_Col1_Returns21Candidates() {
+        Queen queen = new Queen(Color.BLACK);
+        Position position = EasyMock.createMock(Position.class);
+
+        EasyMock.expect(position.getRow()).andStubReturn(8);
+        EasyMock.expect(position.getCol()).andStubReturn(1);
+        EasyMock.replay(position);
+
+        List<Position> actual = queen.getCandidateMoves(position);
+
+        List<Position> expected = List.of(
+                new Position(7,1), new Position(6,1), new Position(5,1), new Position(4,1),
+                new Position(3,1), new Position(2,1), new Position(1,1), new Position(8,2),
+                new Position(8,3), new Position(8,4), new Position(8,5), new Position(8,6),
+                new Position(8,7), new Position(8,8), new Position(7,2), new Position(6,3),
+                new Position(5,4), new Position(4,5), new Position(3,6), new Position(2,7),
+                new Position(1,8)
+        );
+
+        assertEquals(21, actual.size());
+        assertTrue(actual.containsAll(expected));
+    }
 }
