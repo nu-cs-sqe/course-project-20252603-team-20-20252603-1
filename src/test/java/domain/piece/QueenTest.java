b0ef940d99d0e549e60ef56b597240305875ea44
domingoc1232 <152648268+domingoc1232@users.noreply.github.com>
2026-06-01 15:53:41 -0500

    GetCandidateMoves_Black_Row8_Col8_Returns21Candidates passes

--- src/test/java/domain/piece/QueenTest.java
+++ src/test/java/domain/piece/QueenTest.java
@@ -45,11 +45,35 @@ public class QueenTest {
 
         List<Position> expected = List.of(
                 new Position(2,1), new Position(3,1), new Position(4,1), new Position(5,1),
-                new Position(6,1), new Position(7,1), new Position(8,1),
-                new Position(1,2), new Position(1,3), new Position(1,4), new Position(1,5),
-                new Position(1,6), new Position(1,7), new Position(1,8),
-                new Position(2,2), new Position(3,3), new Position(4,4), new Position(5,5),
-                new Position(6,6), new Position(7,7), new Position(8,8)
+                new Position(6,1), new Position(7,1), new Position(8,1), new Position(1,2),
+                new Position(1,3), new Position(1,4), new Position(1,5), new Position(1,6),
+                new Position(1,7), new Position(1,8), new Position(2,2), new Position(3,3),
+                new Position(4,4), new Position(5,5), new Position(6,6), new Position(7,7),
+                new Position(8,8)
+        );
+
+        assertEquals(21, actual.size());
+        assertTrue(actual.containsAll(expected));
+    }
+
+    @Test
+    public void GetCandidateMoves_Black_Row8_Col8_Returns21Candidates() {
+        Queen queen = new Queen(Color.BLACK);
+        Position position = EasyMock.createMock(Position.class);
+
+        EasyMock.expect(position.getRow()).andStubReturn(8);
+        EasyMock.expect(position.getCol()).andStubReturn(8);
+        EasyMock.replay(position);
+
+        List<Position> actual = queen.getCandidateMoves(position);
+
+        List<Position> expected = List.of(
+                new Position(7,8), new Position(6,8), new Position(5,8), new Position(4,8),
+                new Position(3,8), new Position(2,8), new Position(1,8), new Position(8,7),
+                new Position(8,6), new Position(8,5), new Position(8,4), new Position(8,3),
+                new Position(8,2), new Position(8,1), new Position(7,7), new Position(6,6),
+                new Position(5,5), new Position(4,4), new Position(3,3), new Position(2,2),
+                new Position(1,1)
         );
 
         assertEquals(21, actual.size());
