a1b94ebee4402035c24d7299f2fc03b176cabba1
Kavi Subramanyan <kavi.lee.subramanyan@gmail.com>
2026-06-04 15:37:20 -0500

    Make style consistent with other piece classes

--- src/main/java/domain/piece/Bishop.java
+++ src/main/java/domain/piece/Bishop.java
@@ -6,37 +6,24 @@ import java.util.List;
 
 public class Bishop extends Piece {
 
-    private static final int[][] MOVE_VECTORS = { { 1, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 } };
+    private static final int[][] DIRECTIONS = { { 1, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 } };
 
     public Bishop(Color color) {
         super(PieceType.BISHOP, color);
     }
 
-    private List<Position> getCandidateMovesForDirection(int[] direction, Position position) {
-        List<Position> candidates = new ArrayList<Position>();
-
-        int dx = direction[0];
-        int dy = direction[1];
-
-        int row = position.getRow();
-        int col = position.getCol();
-
-        while (Position.validPosition(row + dx, col + dy)) {
-            candidates.add(new Position(row + dx, col + dy));
-
-            row = row + dx;
-            col = col + dy;
-        }
-
-        return candidates;
-    }
-
     public List<Position> getCandidateMoves(Position position) {
         List<Position> candidates = new ArrayList<Position>();
 
-        for (int[] direction : MOVE_VECTORS) {
-            List<Position> newMoves = getCandidateMovesForDirection(direction, position);
-            candidates.addAll(newMoves);
+        for (int[] direction : DIRECTIONS) {
+            int row = position.getRow() + direction[0];
+            int col = position.getCol() + direction[1];
+
+            while (Position.validPosition(row, col)) {
+                candidates.add(new Position(row, col));
+                row += direction[0];
+                col += direction[1];
+            }
         }
 
         return candidates;
