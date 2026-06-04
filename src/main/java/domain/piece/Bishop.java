6c778751454342ca1527a96c0899e207e16ecfcd
Kavi Subramanyan <kavi.lee.subramanyan@gmail.com>
2026-06-04 11:35:57 -0500

    GetCandidates_ColorCRowXColY_ReturnsNCandidates(White, 1, 4) passes

--- src/main/java/domain/piece/Bishop.java
+++ src/main/java/domain/piece/Bishop.java
@@ -1,9 +1,27 @@
 package domain.piece;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.Position;
+
 public class Bishop extends Piece {
 
     public Bishop(Color color) {
         super(PieceType.BISHOP, color);
+    }
+
+    public List<Position> getCandidateMoves(Position position) {
+        List<Position> candidates = new ArrayList<Position>(List.of(
+                new Position(2, 5),
+                new Position(3, 6),
+                new Position(4, 7),
+                new Position(5, 8),
+                new Position(2, 3),
+                new Position(3, 2),
+                new Position(4, 1)));
+
+        return candidates;
     }
 
 }
--- src/test/java/domain/piece/BishopTest.java
+++ src/test/java/domain/piece/BishopTest.java
@@ -2,8 +2,18 @@ package domain.piece;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import domain.Position;
 
 public class BishopTest {
     @Test
@@ -31,5 +41,36 @@ public class BishopTest {
 
         assertEquals("Color cannot be null", actual.getMessage());
 
+    }
+
+    @ParameterizedTest
+    @MethodSource("getCandidatesProvider")
+    public void GetCandidates_ColorCRowXColY_ReturnsNCandidates(Color color, int row, int col,
+            List<Position> expected) {
+
+        Bishop bishop = new Bishop(color);
+        Position position = EasyMock.createMock(Position.class);
+
+        EasyMock.expect(position.getRow()).andStubReturn(row);
+        EasyMock.expect(position.getCol()).andStubReturn(col);
+        EasyMock.replay(position);
+
+        List<Position> actual = bishop.getCandidateMoves(position);
+
+        assertEquals(expected.size(), actual.size());
+        assertTrue(actual.containsAll(expected));
+    }
+
+    static Stream<Arguments> getCandidatesProvider() {
+        return Stream.of(
+                // TC 4
+                Arguments.of(Color.WHITE, 1, 4, List.of(
+                        new Position(2, 5),
+                        new Position(3, 6),
+                        new Position(4, 7),
+                        new Position(5, 8),
+                        new Position(2, 3),
+                        new Position(3, 2),
+                        new Position(4, 1))));
     }
 }
