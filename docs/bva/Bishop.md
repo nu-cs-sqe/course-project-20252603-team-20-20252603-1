8f6edf26689ef3702a9d42a70972e8791ea08c2b
Kavi Subramanyan <kavi.lee.subramanyan@gmail.com>
2026-06-04 11:24:17 -0500

    Constructor_ColorNull_ThrowsIllegalArgumentException passes

--- docs/bva/Bishop.md
+++ docs/bva/Bishop.md
@@ -6,7 +6,7 @@
 | --- | ------------------- | ----------------------------------------------------------- | ------------------ |
 | TC1 | color=`WHITE`       | Bishop created, `getPieceType()`=BISHOP, `getColor()`=WHITE | :white_check_mark: |
 | TC2 | color=`BLACK`       | Bishop created, `getPieceType()`=BISHOP, `getColor()`=BLACK | :white_check_mark: |
-| TC3 | color=`null`        | `IllegalArgumentException` thrown                           | :x:                |
+| TC3 | color=`null`        | `IllegalArgumentException` thrown                           | :white_check_mark: |
 
 ### Method under test: `getCandidateMoves(Position position)`
 
--- src/test/java/domain/piece/BishopTest.java
+++ src/test/java/domain/piece/BishopTest.java
@@ -1,6 +1,7 @@
 package domain.piece;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +20,16 @@ public class BishopTest {
 
         assertEquals(Color.BLACK, bishop.getColor());
         assertEquals(PieceType.BISHOP, bishop.getPieceType());
+    }
+
+    @Test
+    public void Constructor_ColorNull_ThrowsIllegalArgumentException() {
+
+        Exception actual = assertThrows(IllegalArgumentException.class, () -> {
+            new Bishop(null);
+        });
+
+        assertEquals("Color cannot be null", actual.getMessage());
+
     }
 }
