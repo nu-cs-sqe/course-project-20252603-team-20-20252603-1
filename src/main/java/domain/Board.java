96e74f81be6be944407e5fc09e7da8b0fb77ca5e
domingoc1232 <152648268+domingoc1232@users.noreply.github.com>
2026-06-08 10:53:44 -0500

    change unreachable case to throw new exception

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -33,11 +33,11 @@ public class Board {
         switch (type) {
             case PAWN:   return new Pawn(color);
             case KNIGHT: return new Knight(color);
-            case KING: return new King(color);
+            case KING:   return new King(color);
             case BISHOP: return new Bishop(color);
             case ROOK:   return new Rook(color);
             case QUEEN:  return new Queen(color);
-            default:     return new Piece(type, color);
+            default: throw new IllegalStateException("Unhandled piece type: " + type);
         }
     }
 
