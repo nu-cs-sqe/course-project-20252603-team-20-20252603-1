b3442a770665ad30e562407da0783fd4260fa51c
Kavi Subramanyan <kavi.lee.subramanyan@gmail.com>
2026-06-07 21:54:20 -0500

    GetCandidates_ColorCRowXColY_ReturnsNCandidates(WHITE, 1, 4) passes

--- src/main/java/domain/piece/King.java
+++ src/main/java/domain/piece/King.java
@@ -1,9 +1,22 @@
 package domain.piece;
 
+import java.util.List;
+
+import domain.Position;
+
 public class King extends Piece {
 
     public King(Color color) {
         super(PieceType.KING, color);
+    }
+
+    public List<Position> getCandidateMoves(Position position) {
+        return List.of(
+                new Position(2, 4),
+                new Position(1, 3),
+                new Position(1, 5),
+                new Position(2, 5),
+                new Position(2, 3));
     }
 
 }
--- src/test/java/domain/piece/KingTest.java
+++ src/test/java/domain/piece/KingTest.java
@@ -2,8 +2,18 @@ package domain.piece;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import domain.Position;
 
 public class KingTest {
 
@@ -31,6 +41,36 @@ public class KingTest {
         });
 
         assertEquals("Color cannot be null", actual.getMessage());
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("getCandidatesProvider")
+    public void GetCandidates_ColorCRowXColY_ReturnsNCandidates(Color color, int row, int col,
+            List<Position> expected) {
+
+        King king = new King(color);
+        Position position = EasyMock.createMock(Position.class);
+
+        EasyMock.expect(position.getRow()).andStubReturn(row);
+        EasyMock.expect(position.getCol()).andStubReturn(col);
+        EasyMock.replay(position);
+
+        List<Position> actual = king.getCandidateMoves(position);
+
+        assertEquals(expected.size(), actual.size());
+        assertTrue(actual.containsAll(expected));
+    }
+
+    static Stream<Arguments> getCandidatesProvider() {
+        return Stream.of(
+                // TC 4
+                Arguments.of(Color.WHITE, 1, 4, List.of(
+                        new Position(2, 4),
+                        new Position(1, 3),
+                        new Position(1, 5),
+                        new Position(2, 5),
+                        new Position(2, 3))));
 
     }
 }
