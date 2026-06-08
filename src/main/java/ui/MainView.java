6157ffac05bfa89a183824922b783deb5f3dff1f
annabellesole1 <annabellesole1@gmail.com>
2026-06-08 11:49:49 -0500

    add locale supoprt for en_US and es_ES

--- src/main/java/ui/MainView.java
+++ src/main/java/ui/MainView.java
@@ -3,6 +3,9 @@ package ui;
 import domain.piece.Color;
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.text.MessageFormat;
 
 public class MainView extends JFrame {
 
@@ -13,10 +16,14 @@ public class MainView extends JFrame {
     private GameStatsView gameStatsView;
     private String player1Name;
     private String player2Name;
+    private Locale locale;
+    private ResourceBundle bundle;
 
-    public MainView(String player1Name, String player2Name) {
+    public MainView(String player1Name, String player2Name, Locale locale) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        this.locale = locale;
+        this.bundle = ResourceBundle.getBundle("labels/labels", locale);
 
         configureMainView();
         addGameStatsView();
@@ -27,14 +34,15 @@ public class MainView extends JFrame {
     }
 
     private void configureMainView() {
-        setTitle("Our Chess Game GUI!");
+
+        setTitle(bundle.getString("guiTitle"));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setLocationRelativeTo(null); // Center the window on the screen
     }
 
     private void addGameStatsView() {
-        gameStatsView = new GameStatsView(player1Name, player2Name);
+        gameStatsView = new GameStatsView(player1Name, player2Name, locale);
         add(gameStatsView, BorderLayout.PAGE_START);
     }
 
@@ -42,8 +50,17 @@ public class MainView extends JFrame {
         boardView = new BoardView();
         add(boardView, BorderLayout.CENTER);
         boardView.setOnTurnChanged(turn -> {
-            String name = (turn == Color.WHITE) ? "White" : "Black";
-            gameStatsView.currentPlayerLabel.setText("Current Player: " + name);
+            String white = bundle.getString("white");
+            String black = bundle.getString("black");
+            String currentPlayer = bundle.getString("currentPlayer");
+
+            String name = (turn == Color.WHITE) ? white : black;
+
+            String currentPlayerLabelString = MessageFormat.format("{0}: {1}",
+                currentPlayer, name
+            );
+
+            gameStatsView.currentPlayerLabel.setText(currentPlayerLabelString);
         });
     }
 }
