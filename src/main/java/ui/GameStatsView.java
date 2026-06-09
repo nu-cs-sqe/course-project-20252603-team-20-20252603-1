e2975bf845b77f603f60160c75e68b7368033518
annabellesole1 <annabellesole1@gmail.com>
2026-06-09 17:02:37 -0500

    Update GUI to show clock

--- src/main/java/ui/GameStatsView.java
+++ src/main/java/ui/GameStatsView.java
@@ -17,8 +17,13 @@ public class GameStatsView extends JPanel {
     private Locale locale;
 
     public JLabel currentPlayerLabel;
+    public JLabel whiteTimerLabel;
+    public JLabel blackTimerLabel;
+    private final String whiteLocale;
+    private final String blackLocale;
 
-    public GameStatsView(String player1Name, String player2Name, Locale locale) {
+
+    public GameStatsView(String player1Name, String player2Name, Locale locale, long initialTime) {
         this.locale = locale;
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -33,18 +38,18 @@ public class GameStatsView extends JPanel {
 
         String player = labels.getString("player");
         String team = labels.getString("team");
-        String white = labels.getString("white");
-        String black = labels.getString("black");
+        this.whiteLocale = labels.getString("white");
+        this.blackLocale = labels.getString("black");
 
         String player1Header = MessageFormat.format("\t\t\t{0} 1: {1} ({2}: {3})",
-                player, player1Name, team, white);
+                player, player1Name, team, this.whiteLocale);
 
         JLabel player1Label = new JLabel(player1Header);
         player1Label.setFont(new Font("Arial", Font.BOLD, FONT_SIZE_LABEL));
         player1Label.setForeground(Color.WHITE);
 
         String player2Header = MessageFormat.format("\t\t\t{0} 2: {1} ({2}: {3})",
-                player, player2Name, team, black);
+                player, player2Name, team, this.blackLocale);
 
         JLabel player2Label = new JLabel(player2Header);
 
@@ -58,9 +63,36 @@ public class GameStatsView extends JPanel {
         currentPlayerLabel.setFont(new Font("Arial", Font.BOLD, FONT_SIZE_HEADER));
         currentPlayerLabel.setForeground(Color.WHITE);
 
+        long seconds = initialTime / 1000;
+        String formatted = String.format("%02d:%02d", seconds / 60, seconds % 60);
+        
+        String whiteTimerText = MessageFormat.format("{0}: {1}", this.whiteLocale, formatted);
+        whiteTimerLabel = new JLabel(whiteTimerText);
+        whiteTimerLabel.setFont(new Font("Arial", Font.BOLD, FONT_SIZE_LABEL));
+        whiteTimerLabel.setForeground(java.awt.Color.WHITE);
+
+        String blackTimerText = MessageFormat.format("{0}: {1}", this.blackLocale, formatted);
+        blackTimerLabel = new JLabel(blackTimerText);
+        blackTimerLabel.setFont(new Font("Arial", Font.BOLD, FONT_SIZE_LABEL));
+        blackTimerLabel.setForeground(java.awt.Color.WHITE);
+
         add(playerInfoLabel);
         add(player1Label);
         add(player2Label);
         add(currentPlayerLabel);
+        add(whiteTimerLabel);
+        add(blackTimerLabel);
+    }
+
+    public void updateTimer(domain.piece.Color color, long timeRemainingMillis) {
+        long seconds = timeRemainingMillis / 1000;
+        String formatted = String.format("%02d:%02d", seconds / 60, seconds % 60);
+        if (color == domain.piece.Color.WHITE) {
+            String newWhiteTimerText = MessageFormat.format("{0}: {1}", this.whiteLocale, formatted);
+            whiteTimerLabel.setText(newWhiteTimerText);
+        } else {
+            String newBlackTimerText = MessageFormat.format("{0}: {1}", this.blackLocale, formatted);
+            blackTimerLabel.setText(newBlackTimerText);
+        }
     }
 }
--- src/main/java/ui/MainView.java
+++ src/main/java/ui/MainView.java
@@ -1,5 +1,6 @@
 package ui;
 
+import domain.ClockListener;
 import domain.piece.Color;
 import java.awt.BorderLayout;
 import java.text.MessageFormat;
@@ -7,13 +8,14 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 import javax.swing.JFrame;
 
-public class MainView extends JFrame {
+public class MainView extends JFrame implements BoardChangeListener, ClockListener {
 
     private static final int WINDOW_WIDTH = 800;
     private static final int WINDOW_HEIGHT = 1200;
 
     private BoardView boardView;
     private GameStatsView gameStatsView;
+    private BoardController boardController;
     private String player1Name;
     private String player2Name;
     private Locale locale;
@@ -42,24 +44,40 @@ public class MainView extends JFrame {
     }
 
     private void addGameStatsView() {
-        gameStatsView = new GameStatsView(player1Name, player2Name, locale);
+        // TODO: replace initialtime with user preference
+        gameStatsView = new GameStatsView(player1Name, player2Name, locale, 300000);
         add(gameStatsView, BorderLayout.PAGE_START);
     }
 
     private void addBoardView() {
-        boardView = new BoardView();
+        boardController = new BoardController(this, this);
+        boardController.startGame(300000); // TODO replace with user selection
+        boardView = new BoardView(boardController);
         add(boardView, BorderLayout.CENTER);
-        boardView.setOnTurnChanged(turn -> {
-            String white = bundle.getString("white");
-            String black = bundle.getString("black");
-            String currentPlayer = bundle.getString("currentPlayer");
+    }
 
-            String name = (turn == Color.WHITE) ? white : black;
+    @Override
+    public void onBoardChanged() {
+        Color turn = boardController.getCurrentTurn();
+        String white = bundle.getString("white");
+        String black = bundle.getString("black");
+        String currentPlayer = bundle.getString("currentPlayer");
 
-            String currentPlayerLabelString = MessageFormat.format("{0}: {1}",
-                    currentPlayer, name);
+        String name = (turn == Color.WHITE) ? white : black;
+        String currentPlayerLabelString = MessageFormat.format("{0}: {1}", currentPlayer, name);
+        gameStatsView.currentPlayerLabel.setText(currentPlayerLabelString);
+        boardView.repaint();
+    }
 
-            gameStatsView.currentPlayerLabel.setText(currentPlayerLabelString);
-        });
+    @Override
+    public void onTimerTick(Color color, long timeRemainingMillis) {
+        System.out.println("tick: " + color + " " + timeRemainingMillis);
+
+        gameStatsView.updateTimer(color, timeRemainingMillis);
+    }
+
+    @Override
+    public void onTimeout(Color color) {
+        boardController.handleTimeout(color);
     }
 }
