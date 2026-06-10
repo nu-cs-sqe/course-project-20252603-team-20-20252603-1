87986f390b96dd8a8967b3885993c37b0f78e3de
Kavi Subramanyan <kavi.lee.subramanyan@gmail.com>
2026-06-09 19:26:09 -0500

    <player> is in check by one piece #1 passes

---
+++ src/test/java/domain/ChessSteps.java
@@ -0,0 +1,72 @@
+package domain;
+
+import domain.piece.Bishop;
+import domain.piece.Color;
+import domain.piece.King;
+import domain.piece.Knight;
+import domain.piece.Pawn;
+import domain.piece.Piece;
+import domain.piece.PieceType;
+import domain.piece.Queen;
+import domain.piece.Rook;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ChessSteps {
+    Board board;
+    Color player;
+    boolean inCheck;
+
+    private Piece createPiece(PieceType type, Color color) {
+        switch (type) {
+            case PAWN:
+                return new Pawn(color);
+            case KNIGHT:
+                return new Knight(color);
+            case KING:
+                return new King(color);
+            case BISHOP:
+                return new Bishop(color);
+            case ROOK:
+                return new Rook(color);
+            case QUEEN:
+                return new Queen(color);
+            default:
+                throw new IllegalStateException("Unhandled piece type: " + type);
+        }
+    }
+
+    @Given("the game is in progress")
+    public void the_game_is_in_progress() {
+        board = new Board();
+    }
+
+    @Given("the {string} King is at row {int} col {int}")
+    public void the_king_is_at_row_col(String player, Integer kingRow, Integer kingCol) {
+        Position position = new Position(kingRow, kingCol);
+        this.player = Color.valueOf(player);
+        board.placePieceAt(position, createPiece(PieceType.KING, this.player));
+    }
+
+    @Given("a {string} {string} is at row {int} col {int}")
+    public void a_is_at_row_col(String color, String pieceType, Integer row, Integer col) {
+        Position position = new Position(row, col);
+        Piece piece = createPiece(PieceType.valueOf(pieceType), Color.valueOf(color));
+        board.placePieceAt(position, piece);
+    }
+
+    @When("I check if the player is in check")
+    public void i_check_if_the_player_is_in_check() {
+        inCheck = board.isInCheck(this.player);
+    }
+
+    @Then("the player is in check")
+    public void the_player_is_in_check() {
+        assertTrue(inCheck);
+    }
+
+}
---
+++ src/test/resources/features/DetectCheck.feature
@@ -0,0 +1,15 @@
+Feature: Detect Check
+    In order to win the game,
+    as a player,
+    I want to know when I am in check.
+
+    Scenario Outline: <player> is in check by one piece
+        Given the game is in progress
+        And the <player> King is at row <kingRow> col <kingCol>
+        And a <opposingPlayer> <attackingPiece> is at row <attackerRow> col <attackerCol>
+        When I check if the player is in check
+        Then the player is in check
+
+        Examples:
+            | player  | kingRow | kingCol | opposingPlayer | attackingPiece | attackerRow | attackerCol |
+            | "WHITE" | 1       | 4       | "BLACK"        | "ROOK"         | 8           | 4           |
