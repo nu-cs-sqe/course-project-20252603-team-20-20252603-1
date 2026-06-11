489a04df34179a6b730d10a8b6826e92a2947649
annabellesole1 <annabellesole1@gmail.com>
2026-06-11 12:25:15 -0500

    promotion by moving to back rank #1 passes

---
+++ src/test/java/domain/PawnPromotion.java
@@ -0,0 +1,59 @@
+package domain;
+
+import domain.piece.Color;
+import domain.piece.King;
+import domain.piece.Pawn;
+import domain.piece.Piece;
+import domain.piece.PieceType;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;;
+
+public class PawnPromotion {
+    ChessSteps steps;
+    Position fromPosition;
+    Position toPosition;
+    PieceType selected;
+
+    public PawnPromotion(ChessSteps steps) {
+        this.steps = steps;
+    }
+
+    @Given("a {string} Pawn is at row {int} col {int}")
+    public void a_pawn_is_at_row_col(String player, Integer row, Integer col) {
+        this.fromPosition = new Position(row, col);
+        this.steps.player = Color.valueOf(player);
+        this.steps.board.placePieceAt(fromPosition, new Pawn(this.steps.player));
+    }
+
+    @When("{string} moves the pawn to row {int} col {int}")
+    public void moves_the_pawn_to_row_col(String player, Integer row, Integer col) {
+        // Silence locateKing exception
+        Position kingPosition = new Position(4,4);
+        this.steps.board.placePieceAt(kingPosition, new King(this.steps.player));
+        
+        this.toPosition = new Position(row, col);
+        this.steps.board.movePiece(this.fromPosition, this.toPosition);
+    }
+
+    @When("{string} selects {string} as the promotion piece")
+    public void selects_as_the_promotion_piece(String player, String promotionPiece) {
+        this.selected = PieceType.valueOf(promotionPiece);
+        this.steps.board.promotePawn(this.toPosition, selected);
+    }
+
+    @Then("the pawn is removed from row {int} col {int}")
+    public void the_pawn_is_removed_from_row_col(Integer int1, Integer int2) {
+        Piece pieceAtDest = this.steps.board.getPieceAt(this.toPosition);
+        assertFalse(pieceAtDest.getPieceType() == PieceType.PAWN);
+    }
+
+    @Then("a {string} {string} is placed at row {int} col {int}")
+    public void a_is_placed_at_row_col(String string, String string2, Integer int1, Integer int2) {
+        Piece pieceAtDest = this.steps.board.getPieceAt(this.toPosition);
+        assertTrue(pieceAtDest.getPieceType() == this.selected);
+    }
+}
---
+++ src/test/resources/features/PawnPromotion.feature
@@ -0,0 +1,16 @@
+Feature: Pawn Promotion
+    In order to win the game,
+    as a player,
+    I want to know when I am in check.
+
+    Scenario Outline: <player> pawn promotes by moving to back rank
+        Given the game is in progress
+        And a <player> Pawn is at row <fromRow> col <pawnCol>
+        When <player> moves the pawn to row <toRow> col <pawnCol>
+        And <player> selects <promotionPiece> as the promotion piece
+        Then the pawn is removed from row <toRow> col <pawnCol>
+        And a <player> <promotionPiece> is placed at row <toRow> col <pawnCol>
+
+        Examples:
+            | player  | fromRow | toRow | pawnCol | promotionPiece | 
+            | "WHITE" |    7    |   8   |    1    |     "QUEEN"    | 
